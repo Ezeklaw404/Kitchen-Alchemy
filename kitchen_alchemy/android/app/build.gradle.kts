69cec82f267da95c6d7721d458381d30fb9d7540
unknown <zekeandreason@gmail.com>
2025-10-10 18:26:26 -0600

    updated the com.example to be more personal

--- kitchen_alchemy/android/app/build.gradle.kts
+++ kitchen_alchemy/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.kitchen_alchemy"
+    namespace = "com.ezeklawapps.kitchenalchemy.kitchen_alchemy"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.kitchen_alchemy"
+        applicationId = "com.ezeklawapps.kitchenalchemy.kitchen_alchemy"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- kitchen_alchemy/android/app/src/main/kotlin/com/ezeklawapps/kitchenalchemy/kitchen_alchemy/MainActivity.kt
+++ kitchen_alchemy/android/app/src/main/kotlin/com/ezeklawapps/kitchenalchemy/kitchen_alchemy/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.kitchen_alchemy
+package com.ezeklawapps.kitchenalchemy.kitchen_alchemy
 
 import io.flutter.embedding.android.FlutterActivity
 
